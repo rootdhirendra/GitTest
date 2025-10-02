f3cf64ee2b2e23a8c9c9f5146c24fee5490528b8
rootdhirendra <rootdhirendra@gmail.com>
2025-10-02 13:46:28 +0530

    Update ErrorViewModel.cs

--- Models/ErrorViewModel.cs
+++ Models/ErrorViewModel.cs
@@ -3,6 +3,7 @@ namespace GitTest.Models
     public class ErrorViewModel
     {
         public string? RequestId { get; set; }
+        //added on remote
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
